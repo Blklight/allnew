41efa7edb82b55112e671b7a6bce5aad1f67cf90
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2025-03-17 02:49:35 -0300

    rendering multiple cards layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { NavToolbar } from "@/components/nav-toolbar";
-import {
-  BackgroundCard,
-  BasicCard,
-  cardData,
-  SocialCard,
-} from "@/components/styling";
+
 import { IconSidebar } from "@/components/icon-sidebar";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
@@ -20,10 +15,18 @@ import { Bookmark, Clock, ExternalLink } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import BookmarkButton from "@/components/bookmark-comp";
 import { ArticleCard } from "@/components/article-card";
-import { BackgroundCard as NewBackgroundCard } from "@/components/background-card";
+import { BackgroundCard } from "@/components/background-card";
 import { SimpleCard } from "@/components/simple-card";
 import { getDocuments } from "@/services";
 import { TutorialFrame } from "@/components/tutorial-frame";
+
+type CardLayoutType = "ArticleCard" | "BackgroundCard" | "SimpleCard";
+
+const componentMap: Record<CardLayoutType, React.ComponentType<any>> = {
+  ArticleCard,
+  BackgroundCard,
+  SimpleCard,
+};
 
 export default async function Home() {
   const data = await getDocuments();
@@ -43,16 +46,15 @@ export default async function Home() {
       </div>
 
       <div className="grid md:grid-cols-3 grid-cols-1 gap-4 mb-4">
-        {/* <ArticleCard data={cardData} /> */}
-        {/* <NewBackgroundCard data={} /> */}
-        {/* <BasicCard data={cardData} />
-        <SocialCard data={cardData} />
-        <BackgroundCard data={cardData} /> */}
-        {/* <SimpleCard data={cardData} /> */}
         {data.map((doc) => {
-          console.log(doc);
+          console.log(doc.document.stylesheet.cardLayout);
+          const cardLayout = doc.document.stylesheet
+            .cardLayout as CardLayoutType;
+
+          // Verifica se o cardLayout é válido, senão usa um fallback
+          const CardComponent = componentMap[cardLayout] || SimpleCard;
           return (
-            <NewBackgroundCard
+            <CardComponent
               key={doc.document.slug}
               data={doc.document}
               authors={doc.authordetails}
